1e00c4f8888e994412e9dc734a4d136e1e51557e
Richard Anabeto Opoku <anabeto93@gmail.com>
2020-10-02 10:49:59 +0100

    completed project.

---
+++ lib/widgets/user_card.dart
@@ -0,0 +1,37 @@
+import 'package:facebook/models/models.dart';
+import 'package:facebook/widgets/profile_avatar.dart';
+import 'package:flutter/material.dart';
+
+class UserCard extends StatelessWidget {
+  final User user;
+
+  const UserCard({
+    Key key,
+    @required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () { print ('Desktop tapped'); },
+          child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ProfileAvatar(
+            imageUrl: user.imageUrl,
+          ),
+          const SizedBox(
+            width: 6.0,
+          ),
+          Flexible(
+                      child: Text(
+              user.name,
+              style: const TextStyle(fontSize: 16.0),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
